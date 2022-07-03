233f908587e44520f2b158f869ebb1150df38b27
Daniel Stankiewicz <traitorablepl@gmail.com>
2022-07-03 18:42:53 +0200

    Decrease TX advertising interval

--- AoA_Tag_Tx/src/main.c
+++ AoA_Tag_Tx/src/main.c
@@ -40,8 +40,8 @@ static struct bt_le_ext_adv_start_param ext_adv_start_param = {
 };
 
 const static struct bt_le_per_adv_param per_adv_param = {
-    .interval_min = BT_GAP_ADV_SLOW_INT_MIN,
-    .interval_max = BT_GAP_ADV_SLOW_INT_MAX,
+    .interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
+    .interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
     .options = BT_LE_ADV_OPT_USE_TX_POWER,
 };
 
